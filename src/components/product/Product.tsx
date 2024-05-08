fdfdc5e53b6fbf39a3d628e761c9c406a662a7c7
РС <KasiaKasilov@gmail.com>
2024-05-08 18:53:18 +0300

    css changes

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -18,32 +18,26 @@ export interface IProductProps {
 }
 
 type IProductTypeProps = IProductProps & { children?: React.ReactNode }
-const Product: FC<IProductTypeProps> = ({title, description, price,images}) => {
+const Product: FC<IProductTypeProps> = ({title, description, price, images}) => {
 
     return (
         <div className={style.flex_wrapper}>
-<div>
-    <h2>{title}</h2>
-    <div>price: {price}</div>
-<p>{description}</p>
-</div>
-            <Carousel className={style.carousel}>
+            <div className={style.info}>
+                <h2>{title}</h2>
+                <div>price: {price}</div>
+                <p>{description}</p>
+            </div>
+            <Carousel variant='dark' className={style.carousel}>
 
                 {
                     images?.map((image, index) =>
-                        <CarouselItem key={index} >
-                            <img className ="d-block w-100"  src={image} alt={title}/>
+                        <CarouselItem key={index}>
+                            <img className={style.img} src={image} alt={title}/>
                         </CarouselItem>)
                 }
 
             </Carousel>
 
-
-            {/*<div>*/}
-            {/*    {*/}
-            {/*        images?.map((image,index)=> <img key={index} src={image} alt={title}/>)*/}
-            {/*    }*/}
-            {/*</div>*/}
         </div>
     );
 };
