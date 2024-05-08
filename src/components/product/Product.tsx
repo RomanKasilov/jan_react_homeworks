09bb0eda15df0aeedb13765145cb8326e40ae76d
РС <KasiaKasilov@gmail.com>
2024-05-08 14:36:38 +0300

    +bootstrap Carousel

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,4 +1,7 @@
 import React, {FC} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import style from './Product.module.css'
+import {Carousel, CarouselItem} from "react-bootstrap";
 
 export interface IProductProps {
     "id": number,
@@ -18,17 +21,29 @@ type IProductTypeProps = IProductProps & { children?: React.ReactNode }
 const Product: FC<IProductTypeProps> = ({title, description, price,images}) => {
 
     return (
-        <div>
+        <div className={style.flex_wrapper}>
 <div>
     <h2>{title}</h2>
-    <div>{price}</div>
+    <div>price: {price}</div>
 <p>{description}</p>
 </div>
-            <div>
+            <Carousel className={style.carousel}>
+
                 {
-                    images?.map((image,index)=> <img key={index} src={image} alt={title}/>)
+                    images?.map((image, index) =>
+                        <CarouselItem key={index} >
+                            <img className ="d-block w-100"  src={image} alt={title}/>
+                        </CarouselItem>)
                 }
-            </div>
+
+            </Carousel>
+
+
+            {/*<div>*/}
+            {/*    {*/}
+            {/*        images?.map((image,index)=> <img key={index} src={image} alt={title}/>)*/}
+            {/*    }*/}
+            {/*</div>*/}
         </div>
     );
 };
