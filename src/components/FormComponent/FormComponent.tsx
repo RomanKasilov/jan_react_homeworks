5f0598b5766ffb1c377b9cd88c4803221e716c07
РС <KasiaKasilov@gmail.com>
2024-05-20 14:04:35 +0300

    joi validator debugger

--- src/components/FormComponent/FormComponent.tsx
+++ src/components/FormComponent/FormComponent.tsx
@@ -1,8 +1,8 @@
 import {useForm} from "react-hook-form";
+import {joiResolver} from "@hookform/resolvers/joi";
+
 import {ISavePostModel} from "../../models/savePostModel";
 import {postService} from "../../services/json.api.service";
-
-import {joiResolver} from "@hookform/resolvers/joi";
 import {savePostValidator} from "../../validators/savePost.validator";
 
 
@@ -12,27 +12,26 @@ const FormComponent = () => {
         handleSubmit,
         formState: {errors, isValid}
     } = useForm<ISavePostModel>(
-        {mode: "onSubmit", resolver: joiResolver(savePostValidator)})
+        {mode: "onTouched", resolver: joiResolver(savePostValidator)})
 
     const savePost = (data: ISavePostModel) => {
-        postService.savePost(data).then(value => console.log(value.data))
+        postService.savePost(data)
+            .then(value => console.log(value.data))
     };
     return (
         <>
             <form onSubmit={handleSubmit(savePost)}>
-
                 <input type="number" placeholder={"userId"} {...register('userId')}/>
                 {errors.userId && <span>{errors.userId.message}</span>}
 
-
                 <input type="text" placeholder={"title"}{...register('title')}/>
                 {errors.title && <span>{errors.title.message}</span>}
-
 
                 <input type="text" placeholder={"body"}{...register('body')}/>
                 {errors.body && <span>{errors.body.message}</span>}
 
                 <button disabled={!isValid}>save Post</button>
+
             </form>
         </>
     );
