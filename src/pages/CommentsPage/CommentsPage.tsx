95421055c23e3f0a086c9d2c1587f35517b2ae65
РС <KasiaKasilov@gmail.com>
2024-05-26 14:35:39 +0300

    css button

--- src/pages/CommentsPage/CommentsPage.tsx
+++ src/pages/CommentsPage/CommentsPage.tsx
@@ -19,7 +19,7 @@ const CommentsPage = () => {
         <div className={css.wrapper}>
             <div className={css.subHeader}>
                 <h2>Some comments by some Users....</h2>
-                <button onClick={() => navigateBack(-1)}>Back to User's posts</button>
+                <button onClick={() => navigateBack(-1)}>Back to User's Posts</button>
             </div>
             <Comments comments={comments}/>
             <button className={css.backToUsersButton} onClick={() => navigateToUsers('/users')}>
